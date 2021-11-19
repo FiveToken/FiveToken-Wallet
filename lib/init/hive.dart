621bc11112d26e4645955e3595946eb2fdc4a6a4
zhupengcong <zhupengcong@ipfsforce.com>
2021-11-19 10:29:26 +0800

    feal/ hive encrypt

--- lib/init/hive.dart
+++ lib/init/hive.dart
@@ -2,12 +2,14 @@ import 'package:fil/chain/gas.dart';
 import 'package:fil/chain/net.dart';
 import 'package:fil/chain/token.dart';
 import 'package:fil/chain/wallet.dart';
+import 'package:fil/index.dart';
 import 'package:fil/models/address.dart';
 import 'package:fil/models/cacheMessage.dart';
 import 'package:fil/models/nonce.dart';
 import 'package:fil/models/wallet.dart';
 import 'package:hive/hive.dart';
 import 'package:hive_flutter/hive_flutter.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 
 const messageBox = 'message';
 const addressBox = 'address';
@@ -18,6 +20,7 @@ const netBox = 'netBox';
 const tokenBox = 'tokenBox';
 const walletBox = 'walletBox';
 const cacheMessageBox = 'cacheMessageBox';
+
 Future initHive() async {
   await Hive.initFlutter();
   Hive.registerAdapter(WalletAdapter());
@@ -29,14 +32,24 @@ Future initHive() async {
   Hive.registerAdapter(ContactAddressAdapter());
   Hive.registerAdapter(CacheMessageAdapter());
   Hive.registerAdapter(ChainGasAdapter());
-  await Hive.openBox<Wallet>(addressBox);
-  await Hive.openBox<ContactAddress>(addressBookBox);
-  await Hive.openBox<Nonce>(nonceBox);
-  await Hive.openBox<ChainGas>(gasBox);
-  await Hive.openBox<Network>(netBox);
-  await Hive.openBox<Token>(tokenBox);
-  await Hive.openBox<ChainWallet>(walletBox);
-  await Hive.openBox<CacheMessage>(cacheMessageBox);
+
+
+  final FlutterSecureStorage secureStorage = const FlutterSecureStorage();
+  final String hiveKey = Global.uuid;
+  var containsEncryptionKey =  await secureStorage.containsKey(key: hiveKey);
+  if(!containsEncryptionKey){
+    var key = Hive.generateSecureKey();
+    await secureStorage.write(key: hiveKey, value: base64UrlEncode(key));
+  }
+  var encryptionKey = base64Url.decode(await secureStorage.read(key: hiveKey));
+  await Hive.openBox<Wallet>(addressBox, encryptionCipher: HiveAesCipher(encryptionKey));
+  await Hive.openBox<ContactAddress>(addressBookBox, encryptionCipher: HiveAesCipher(encryptionKey));
+  await Hive.openBox<Nonce>(nonceBox, encryptionCipher: HiveAesCipher(encryptionKey));
+  await Hive.openBox<ChainGas>(gasBox, encryptionCipher: HiveAesCipher(encryptionKey));
+  await Hive.openBox<Network>(netBox, encryptionCipher: HiveAesCipher(encryptionKey));
+  await Hive.openBox<Token>(tokenBox, encryptionCipher: HiveAesCipher(encryptionKey));
+  await Hive.openBox<ChainWallet>(walletBox, encryptionCipher: HiveAesCipher(encryptionKey));
+  await Hive.openBox<CacheMessage>(cacheMessageBox, encryptionCipher: HiveAesCipher(encryptionKey));
   // await OpenedBox.walletInstance.deleteFromDisk();
   // await OpenedBox.mesInstance.deleteFromDisk();
   // await OpenedBox.tokenInstance.deleteFromDisk();
@@ -47,15 +60,10 @@ class OpenedBox {
   static Box<Wallet> addressInsance;
   static Box<ContactAddress> addressBookInsance;
   static Box<Nonce> nonceInsance;
-
   static Box<ChainGas> gasInsance;
-
   static Box<Network> netInstance;
-
   static Box<Token> tokenInstance;
-
   static Box<ChainWallet> walletInstance;
-
   static Box<CacheMessage> mesInstance;
 
   static void initBox() {
