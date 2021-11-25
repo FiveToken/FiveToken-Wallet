73b783b2e78a5cb78ed2019f1a2e138b0d051b66
zhupengcong <zhupengcong@ipfsforce.com>
2021-11-25 15:59:29 +0800

    update encryptKey

--- lib/chain/key.dart
+++ lib/chain/key.dart
@@ -1,3 +1,25 @@
+import 'dart:typed_data';
+
+import 'package:fil/store/store.dart';
+
+class EncryptArgon {
+
+   EncryptKey encryptKey;
+   Uint8List argon2Key;
+
+  EncryptArgon({this.encryptKey,this.argon2Key});
+
+  dynamic getKey () {
+    switch($store.encryptionType.value){
+      case 'argon2':
+       return argon2Key;
+        break;
+      default:
+        return encryptKey;
+    }
+  }
+}
+
 class EncryptKey {
   String kek;
   String digest;
