a257ded5b7417caf790cdc5d938878999796e696
zhupengcong <zhupengcong@ipfsforce.com>
2021-12-09 15:22:44 +0800

    feal/ global time

---
+++ test/common/time_test.dart
@@ -0,0 +1,10 @@
+import 'package:fil/index.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  test("generate time", () async {
+    var str = await formatTimeByStr(1639018100);
+    print(str);
+    expect(str, equals('2021-12-09 10:48:20'));
+  });
+}
