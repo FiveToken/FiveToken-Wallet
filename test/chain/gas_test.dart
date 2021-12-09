97bebd8c6cb16260818582cbbca94441e33af7f7
吴博文 <wubowen@ipfsforce.com>
2021-12-09 10:54:13 +0800

    fix:测试用例修改

--- test/chain/gas_test.dart
+++ test/chain/gas_test.dart
@@ -12,12 +12,6 @@ void main() {
     test("test normal gas", () {
       expect(BigInt.parse(gas.handlingFee), BigInt.from(300000));
     });
-    // test("test fast gas", () {
-    //   expect(gas.fast.feeNum, BigInt.from(330000));
-    // });
-    // test("test slow gas", () {
-    //   expect(gas.slow.feeNum, BigInt.from(270000));
-    // });
     test("test max fee", () {
       var fee = '0.00029 nanoFIL';
       expect(gas.handlingFee, fee);
