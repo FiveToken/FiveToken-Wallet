5765456938db5779f12ef6a8606f3f7bb31e042b
zhupengcong <zhupengcong@ipfsforce.com>
2021-12-15 15:30:08 +0800

    feal/ update bloc net

--- test/bloc/net_test.dart
+++ test/bloc/net_test.dart
@@ -1,21 +1,22 @@
 import 'package:fil/bloc/net/net_bloc.dart';
 import 'package:bloc_test/bloc_test.dart';
+import 'package:fil/init/hive.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:fil/chain/net.dart';
+import 'package:mockito/mockito.dart';
+import '../box.dart';
 void main() {
-  List<List<Network>> net = Network.netList;
   group('netBloc',(){
-     NetBloc netBloc;
-
-    setUp((){
-      netBloc = NetBloc();
-    });
-
+    OpenedBox.netInstance = MockBox<Network>();
+    when(OpenedBox.netInstance.values).thenReturn([]);
+    print(OpenedBox.netInstance.values);
+    NetBloc netBloc = NetBloc();
+    List<List<Network>> net = Network.netList;
     blocTest(
       'emit when',
       build: ()=> netBloc,
       act: (bloc) => bloc.add(SetNetEvent(net)),
-      expect: ()=> [net]
+      expect: ()=> [NetState(network: net)]
     );
   });
 }
