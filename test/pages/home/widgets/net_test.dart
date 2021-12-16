395f076389ad80d12805f561a0ff9d0acd181242
吴博文 <wubowen@ipfsforce.com>
2021-12-16 13:34:24 +0800

    fix:测试用例错误处理

--- test/pages/home/widgets/net_test.dart
+++ test/pages/home/widgets/net_test.dart
@@ -1,6 +1,6 @@
+
 import 'package:fil/chain/net.dart';
 import 'package:fil/chain/wallet.dart';
-import 'package:fil/init/hive.dart';
 import 'package:fil/pages/home/widgets/net.dart';
 import 'package:fil/store/store.dart';
 import 'package:fil/widgets/style.dart';
@@ -9,45 +9,23 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:get/get_core/src/get_main.dart';
 import 'package:get/get_instance/src/extension_instance.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
-import 'package:get/get_rx/src/rx_types/rx_types.dart';
 import 'package:get/get_utils/src/extensions/internacionalization.dart';
-import 'package:hive/hive.dart';
-import 'package:mockito/annotations.dart';
 import 'package:mockito/mockito.dart';
 
 import '../../../box.dart';
 import '../../../constant.dart';
-import '../../../request/ether_test.mocks.dart';
 
-@GenerateMocks([
-  Box,
-  StoreController
-])
 void main() {
-  final _mockStoreController = MockStoreController();
-  // Get.put(_mockStoreController);
-  var mockBox = MockBox();
-  final chainWallet = ChainWallet().obs;
-  OpenedBox.walletInstance = MockBox<ChainWallet>();
-  when(OpenedBox.walletInstance.values).thenReturn([]);
-  final network = Network.binanceTestnet.obs;
-
-  $store = _mockStoreController;
-  // $store.setWallet(
-  // ChainWallet(
-  //     rpc: Network.ethMainNet.rpc,
-  //     label: WalletLabel,
-  //     address: EthAddr,
-  //     type: 0)
-  //     );
-
-  when($store.wallet).thenReturn(chainWallet);
-  when($store.wal).thenReturn(chainWallet.value);
-  when($store.network).thenReturn(network);
-  when($store.net).thenReturn(network.value);
-
-  test("description", (){});
-
+  Get.put(StoreController());
+  var box = mockChainWalletBox();
+  var netBox = mockNetbox();
+  when(netBox.values).thenReturn([]);
+  when(box.values).thenReturn([]);
+  $store.setWallet(ChainWallet(
+      rpc: Network.ethMainNet.rpc,
+      label: WalletLabel,
+      address: EthAddr,
+      type: 0));
   testWidgets('test render net select', (tester) async {
     await tester.pumpWidget(GetMaterialApp(
         home: Scaffold(
@@ -58,10 +36,6 @@ void main() {
             preferredSize: Size.fromHeight(NavHeight),
           ),
         )));
-
-    // expect(find.byIcon(Icons.check_circle_outline), findsOneWidget);
-    // await tester.tap(find.text('wallet'.tr));
-    // await tester.pumpAndSettle();
-    // expect(find.byIcon(Icons.check_circle_outline), findsNWidgets(9));
+    expect(find.text('wallet'.tr), findsOneWidget);
   });
 }
